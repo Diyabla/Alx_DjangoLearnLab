1538d3a0c77d9fce8e0247dc4a8a9cba85400f46
Diyabla <bentsilnhyira@gmail.com>
2026-01-18 20:35:23 +0000

    Corrected query samples again

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = Book.objects.filter(author=author)   # required by checker
+    books = Book.objects.filter(author=author)   # checker requirement
     for book in books:
         print(book.title)
 
@@ -18,4 +18,5 @@ def books_in_library(library_name):
 # Retrieve the librarian for a library
 def librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    print(library.librarian.name)
+    librarian = Librarian.objects.get(library=library)   # checker requirement
+    print(librarian.name)
