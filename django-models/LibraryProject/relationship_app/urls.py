00418bb06ad6d70ce093710caffd256482001e4f
Diyabla <bentsilnhyira@gmail.com>
2026-02-01 18:41:56 +0000

    changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,7 +3,14 @@ from . import views
 from views import LibraryDetailView
 from .views import list_books
 
+
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),  # function-based view
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), # class-based view
+    path("", views.index, name="index"),
+    path("books_list/", views.list_books, name="books_list"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+
+    # Authentication URLs
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register_view, name="register"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
 
 # Function-based view: lists all books
 
@@ -19,3 +22,40 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'  # name used in template to access library object
     
+
+
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, f"Welcome, {user.username}!")
+            return redirect("index")  # redirect to home page or books page
+        else:
+            messages.error(request, "Invalid username or password")
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+
+# Logout view
+def logout_view(request):
+    logout(request)
+    return render(request, "relationship_app/logout.html")
+
+# Registration view
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto-login after registration
+            messages.success(request, f"Account created for {user.username}!")
+            return redirect("index")
+        else:
+            messages.error(request, "Please correct the errors below")
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
