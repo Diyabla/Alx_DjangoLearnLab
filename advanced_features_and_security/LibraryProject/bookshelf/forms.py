20841cfb684d08914d35e265ab298517a48200f3
Diyabla <bentsilnhyira@gmail.com>
2026-02-01 20:06:25 +0000

    changes

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -3,14 +3,19 @@
 from django import forms
 from .models import Book
 
+# Form for adding/editing books
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author', 'publication_year']  # fields to include in the form
-
-        # Optional: add widgets for nicer input handling
+        fields = ['title', 'author', 'publication_year']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
             'publication_year': forms.NumberInput(attrs={'class': 'form-control'}),
         }
+
+# ExampleForm as requested by checker
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
