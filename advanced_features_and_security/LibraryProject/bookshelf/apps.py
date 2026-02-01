017776d680d0c02b1a853081a020b7648a148a99
Diyabla <bentsilnhyira@gmail.com>
2026-02-01 19:53:56 +0000

    changes

--- advanced_features_and_security/LibraryProject/bookshelf/apps.py
+++ advanced_features_and_security/LibraryProject/bookshelf/apps.py
@@ -1,5 +1,39 @@
+# bookshelf/apps.py
 from django.apps import AppConfig
-
+from django.contrib.auth.models import Group, Permission
+from django.db.models.signals import post_migrate
+from django.contrib.contenttypes.models import ContentType
 
 class BookshelfConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookshelf'
+
+    def ready(self):
+        post_migrate.connect(create_groups, sender=self)
+
+
+def create_groups(sender, **kwargs):
+    from .models import Book
+
+    # Define group names
+    groups = ['Admins', 'Editors', 'Viewers']
+
+    for group_name in groups:
+        group, created = Group.objects.get_or_create(name=group_name)
+
+    # Assign permissions
+    content_type = ContentType.objects.get_for_model(Book)
+
+    # Admins: all permissions
+    admin_perms = Permission.objects.filter(content_type=content_type)
+    Group.objects.get(name='Admins').permissions.set(admin_perms)
+
+    # Editors: can_create and can_edit
+    editor_perms = Permission.objects.filter(
+        codename__in=['can_create', 'can_edit']
+    )
+    Group.objects.get(name='Editors').permissions.set(editor_perms)
+
+    # Viewers: can_view only
+    viewer_perms = Permission.objects.filter(codename='can_view')
+    Group.objects.get(name='Viewers').permissions.set(viewer_perms)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from .models import Book
+
+@login_required
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
+@login_required
+@permission_required('bookshelf.can_create', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        author_id = request.POST['author']
+        year = request.POST['publication_year']
+        Book.objects.create(title=title, author_id=author_id, publication_year=year)
+        return redirect('book_list')
+    return render(request, 'bookshelf/add_book.html')
+
+
+@login_required
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.title = request.POST['title']
+        book.publication_year = request.POST['publication_year']
+        book.save()
+        return redirect('book_list')
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
+
+
+@login_required
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    book.delete()
+    return redirect('book_list')
