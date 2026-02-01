8e9d811bdb6d14609c20d46480db65e0df3b6b29
Diyabla <bentsilnhyira@gmail.com>
2026-02-01 19:58:32 +0000

    changes

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,16 @@
+# LibraryProject/bookshelf/forms.py
+
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']  # fields to include in the form
+
+        # Optional: add widgets for nicer input handling
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'publication_year': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
